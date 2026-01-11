abf0017bfb652e7af1c98b933ff1d069f9cef635
Weronika Kansy <weronika.kansy@gmail.com>
2026-01-12 00:53:38 +0100

    output dummy component page

--- src/pages/component/Component.tsx
+++ src/pages/component/Component.tsx
@@ -1,5 +1,17 @@
+import { useParams } from 'react-router';
+
 import styles from './Component.module.css';
 
 export const ComponentPage = () => {
-  return <>Component</>;
+  const { componentId } = useParams();
+
+  const title = componentId
+    ? componentId[0].toUpperCase() + componentId.slice(1)
+    : undefined;
+
+  return (
+    <article>
+      <h2>{title}</h2>
+    </article>
+  );
 };
