5bb38f4a6e5847874ff0534cee1d6944bf656af3
Weronika Kansy <weronika.kansy@gmail.com>
2026-01-12 00:29:33 +0100

    output dummy component list

---
+++ src/data/components.tsx
@@ -0,0 +1,18 @@
+export const COMPONENTS = [
+  {
+    category: 'ui',
+    title: 'button',
+  },
+  {
+    category: 'ui',
+    title: 'link',
+  },
+  {
+    category: 'ui',
+    title: 'input',
+  },
+  {
+    category: 'ui',
+    title: 'icon',
+  },
+];
--- src/pages/components/Components.tsx
+++ src/pages/components/Components.tsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router';
+
+import { COMPONENTS } from '../../data/components';
 import styles from './Components.module.css';
 
 export const CategoryPage = () => {
-  return <>Components</>;
+  const url = '/components/component/';
+
+  const componentList = COMPONENTS.map((c) => (
+    <Link key={c.title} to={`${url}${c.title}`}>
+      <li>{c.title}</li>
+    </Link>
+  ));
+
+  return (
+    <section>
+      <h2>Components:</h2>
+      <ul>{componentList}</ul>
+    </section>
+  );
 };
