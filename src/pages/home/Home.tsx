28d2cd3723069974f5b7e9709b63247718a874ea
Weronika Kansy <weronika.kansy@gmail.com>
2026-01-25 01:14:34 +0100

    sample buttons added to home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,7 @@
+import { Button } from '../../components/button/Button';
+import { StyledButton } from '../../components/button/StyledButton';
+import { heartIcon } from '../../components/icon/heart';
+import { Icon } from '../../components/icon/Icon';
 import { TextInput } from '../../components/input/Input';
 import { SectionWrapper } from '../../components/section-wrapper/SectionWrapper';
 import styles from './Home.module.css';
@@ -5,7 +9,13 @@ import styles from './Home.module.css';
 export const HomePage = () => {
   return (
     <SectionWrapper className={styles.homeWrapper}>
-      <button>Button example</button>
+      <StyledButton label="Add to favorites">
+        <Icon path={heartIcon} size="medium" />
+      </StyledButton>
+      <Button label="Add to favorites">
+        <Icon path={heartIcon} size="medium" />
+      </Button>
+      <StyledButton label="Delete" variant="danger" />
       <p>
         Text with a <a href="#">link example</a>.
       </p>
