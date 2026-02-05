12442da3f15106e06e441899d78f4b0d073420cf
Weronika Kansy <weronika.kansy@gmail.com>
2026-02-05 23:11:17 +0100

    search page updated with mock data fetching

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,20 +1,23 @@
 import { Link, useSearchParams } from 'react-router';
 import { SectionWrapper } from '../../components/common/section-wrapper/SectionWrapper';
-import { type Product } from '../../data/products';
+import { useFetchData } from '../../hooks/useFetchData';
+import { type Product } from '../../types/types';
 import { filterProducts } from '../../helpers/functions';
+import { ENDPOINT } from '../../helpers/constants';
 import styles from './Search.module.css';
 
 export const SearchPage = () => {
+  const [initialProducts] = useFetchData(ENDPOINT);
   const [searchParams] = useSearchParams();
   const query = searchParams.get('q');
 
-  let products: Product[] = [];
+  let filteredProducts: Product[] = [];
 
   if (query) {
-    products = filterProducts(query);
+    filteredProducts = filterProducts(query, initialProducts);
   }
 
-  const productList = products.map((p) => (
+  const productList = filteredProducts.map((p) => (
     <li key={p.title}>
       <Link to={`/products/p/${p.title}`}>{p.title}</Link>
     </li>
@@ -24,7 +27,10 @@ export const SearchPage = () => {
     <section>
       <SectionWrapper className={styles.searchWrapper}>
         <h2>Search results:</h2>
-        <ul>{productList}</ul>
+        {filteredProducts.length > 0 && <ul>{productList}</ul>}
+        {!filteredProducts.length && (
+          <p>Seems that you've searched for... nothing. Try again!</p>
+        )}
       </SectionWrapper>
     </section>
   );
