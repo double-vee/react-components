f378d692c9e66448623495d64dd94e6ac2b8d424
Weronika Kansy <weronika.kansy@gmail.com>
2026-02-05 23:13:02 +0100

    data fetching and debounce logic added to header search and products page

--- src/components/layout/header/HeaderSearch.tsx
+++ src/components/layout/header/HeaderSearch.tsx
@@ -1,20 +1,35 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router';
 import { SearchForm } from '../../common/search/SearchForm';
-import { type Product } from '../../../data/products';
-import { filterProducts } from '../../../helpers/functions';
+import { useFetchData } from '../../../hooks/useFetchData';
+import {
+  debounce,
+  filterProducts,
+  limitProducts,
+} from '../../../helpers/functions';
+import { ENDPOINT } from '../../../helpers/constants';
+import { type Product } from '../../../types/types';
 import styles from './HeaderSearch.module.css';
 
 export const HeaderSearch = () => {
+  const [initialProducts] = useFetchData(ENDPOINT);
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState<Product[] | []>([]);
-
   const navigate = useNavigate();
+
+  const setDebouncedSuggestions = debounce(
+    (input: string, products: Product[]) => {
+      const filtered = filterProducts(input, products);
+      const sliced = limitProducts(filtered);
+
+      setSuggestions(sliced);
+    },
+  );
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const input = e.target.value;
     setQuery(input);
-    setSuggestions(input ? filterProducts(input).slice(0, 6) : []);
+    setDebouncedSuggestions(input, initialProducts);
   }
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -49,7 +64,7 @@ export const HeaderSearch = () => {
         handleChange={handleChange}
         handleSubmit={handleSubmit}
       />
-      {query && <SuggestionList list={suggestionList} />}
+      {suggestions.length > 0 && <SuggestionList list={suggestionList} />}
     </section>
   );
 };
--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,21 +1,26 @@
 import { useState } from 'react';
 import { Link } from 'react-router';
-import { PRODUCTS, type Product } from '../../data/products';
 import { ProductFilter } from './ProductFilter';
 import { SectionWrapper } from '../../components/common/section-wrapper/SectionWrapper';
+import { useDebounceValue } from '../../hooks/useDebounceValue';
+import { useFetchData } from '../../hooks/useFetchData';
 import { filterProducts } from '../../helpers/functions';
+import { type Product } from '../../types/types';
+import { ENDPOINT } from '../../helpers/constants';
 import styles from './Products.module.css';
 
 export const ProductPage = () => {
+  const [initialProducts] = useFetchData(ENDPOINT);
   const [query, setQuery] = useState('');
+  const debouncedQuery = useDebounceValue(query);
 
-  let products: Product[] = PRODUCTS;
+  let filteredProducts: Product[] = initialProducts;
 
-  if (query) {
-    products = filterProducts(query);
+  if (debouncedQuery) {
+    filteredProducts = filterProducts(debouncedQuery, filteredProducts);
   }
 
-  const productList = products.map((p) => (
+  const productList = filteredProducts.map((p) => (
     <li key={p.title}>
       <Link to={`/products/p/${p.title}`}>{p.title}</Link>
     </li>
@@ -26,7 +31,10 @@ export const ProductPage = () => {
       <SectionWrapper className={styles.productsWrapper}>
         <ProductFilter query={query} setQuery={setQuery} />
         <h2>Products:</h2>
-        <ul>{productList}</ul>
+        {filteredProducts.length > 0 && <ul>{productList}</ul>}
+        {!filteredProducts.length && !debouncedQuery && (
+          <p>We couldn't find any books :[</p>
+        )}
       </SectionWrapper>
     </section>
   );
